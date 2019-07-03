793bf8fac79d6b6be41d030107f5006c3458d594
wtee <wtee@users.noreply.github.com>
2019-07-03 16:26:03 -0500

    Remove cdm_collections function

--- arks_code/download_cdm.py
+++ arks_code/download_cdm.py
@@ -1,7 +1,8 @@
-import re
 import os
+import re
 import shutil
 import time
+
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 
@@ -98,78 +99,3 @@ class DownloadXML:
         collections = [o.get_attribute("value") for o in options]
 
         return collections
-
-
-def cdm_collections():
-    """DEPRECATED! Use DownloadXML.get_cdm_collections() to generate a list
-    of current collections."""
-
-    collections = [
-        "/p16001coll16",
-        "/p16001coll13",
-        "/p16001coll3",
-        "/p15031coll11",
-        "/p16001coll1",
-        "/p15031coll20",
-        "/p15031coll9",
-        "/p15031coll5",
-        "/p16001coll7",
-        "/p16001coll26",
-        "/p15031coll12",
-        "/p15031coll14",
-        "/p16001coll28",
-        "/p16001coll27",
-        "/p15031coll26",
-        "/p16001coll18",
-        "/p15031coll22",
-        "/p15031coll3",
-        "/p15031coll10",
-        "/p16001coll10",
-        "/p15031coll7",
-        "/p16001coll5",
-        "/p15031coll6",
-        "/p16001coll15",
-        "/p16001coll2",
-        "/p16001coll29",
-        "/p16001coll30",
-        "/p16001coll21",
-        "/p16001coll22",
-        "/p15031coll18",
-        "/p15031coll24",
-        "/p16001coll17",
-        "/p16001coll8",
-        "/p16001coll14",
-        "/p15031coll19",
-        "/p16001coll6",
-        "/p16001coll11",
-        "/p15031coll21",
-        "/p16001coll9",
-        "/p16001coll12",
-        "/p15031coll15",
-        "/p15031coll17",
-        "/p15031coll16",
-        "/p16001coll4",
-        "/p16001coll19",
-        "/p16001coll32",
-        "/p16001coll33",
-        "/p16001coll34",
-        "/p16001coll36",
-        "/p16001coll20",
-        "/p16001coll23",
-        "/p16001coll24",
-        "/p16001coll25",
-        "/p16001coll31",
-        "/p16001coll35",
-        "/p16001coll37",
-        "/p16001coll38",
-        "/p16001coll39",
-        "/p16001coll40",
-        "/p16001coll41",
-        "/p16001coll42",
-        "/p16001coll43",
-        "/p16001coll44",
-        "/p16001coll45",
-        "/p16001coll46",
-        "/p16001coll47",
-    ]
-    return collections
